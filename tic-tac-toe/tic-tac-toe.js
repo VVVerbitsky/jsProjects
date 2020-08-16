0edc0af1474b3ab3423c2c7ef9dac9de89372ba7
Vadim Verbitsky <4diffspam@mail.ru>
2020-08-16 16:03:16 +0300

    deleted some visual effects and renamed variable noWin->draw

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -16,8 +16,8 @@ var id;
 var positionX,positionY;
 var player=true;
 var pixels=document.getElementsByClassName('pixel');
-var restart=document.getElementById('restart');
-restart.addEventListener('click',clear);
+var clearField=document.getElementById('clearField');
+clearField.addEventListener('click',clear);
 for(var i=0;i<pixels.length;i++){
     pixels[i].addEventListener('click',ticORtac);
 };
@@ -42,8 +42,6 @@ function check(){
         if(pixels[id[0]].innerHTML==pixels[id[1]].innerHTML&&pixels[id[1]].innerHTML==pixels[id[2]].innerHTML&&pixels[id[0]].innerHTML!=''){
             for(i=0;i<3;i++){
                 pixels[id[i]].style.color='red';
-                pixels[id[i]].style.borderRadius='100%';
-                pixels[id[i]].style.transform='rotate(360deg)';
             };
             id=true;
             winTime++;
@@ -58,7 +56,7 @@ function check(){
         }
     };
     if(both==true&&id!=true&&winBothTime==0){
-        alert('no win');
+        alert('draw');
         bothPlayers++;
         winBothTime++;
         var noWin=document.getElementById('bothWin');
@@ -68,8 +66,6 @@ function check(){
 function clear(){
     for(var i=0;i<pixels.length;i++){
         pixels[i].style.color='black';
-        // pixels[i].style.backgroundColor='white';
-        pixels[i].style.borderRadius='0%';
         pixels[i].innerHTML='';
     };
     id=false;
@@ -88,32 +84,5 @@ function statistic(){
             var winO=document.getElementById('playerO');
             winO.innerHTML='player 0 win:'+playerO;
         };
-
-
-
-    }
-
-
-
-    
+    }    
 };
-
-
-
-// var player=function play(){
-// // who==true?(return:true,who=false):(this.innerHTML='0',player=true)):(alert(this.positionX+':'+this.positionY));
-// if()
-// }
-
-
-
-// function check(){
-//     // for(var j=0;j<winIndex;j++){
-//     //     var winPixels=winIndex[j];
-//     //     if(pixels[winPixels[0]])
-//     // }
-// }
-// function ticORtac(){
-// this.innerHTML==''?(player==true?(this.innerHTML='X',player=false):(this.innerHTML='0',player=true)):(alert(this.positionX+':'+this.positionY));
-// check();
-// }
