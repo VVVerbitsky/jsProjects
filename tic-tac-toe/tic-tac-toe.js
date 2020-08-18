db9797aae7e5f20d2ff6651e7975a313b49a0b5a
Vadim Verbitsky <vvverbits@gmail.com>
2020-08-18 08:10:55 +0300

    renamed many variables, now works with 'use strict',optimized all functions

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,4 +1,5 @@
-const winIndex = [
+"use strict"
+const winningPositions = [
     [0, 1, 2], 
     [3, 4, 5], 
     [6, 7, 8], 
@@ -8,79 +9,85 @@ const winIndex = [
     [0, 4, 8], 
     [2, 4, 6]
 ];
-let bothPlayers=0;
-let winTime=0;
-let winBothTime=0;
-let playerX=0,playerO=0;
-let id;
-let player=true;
-const pixels=document.getElementsByClassName('pixel');
-const clearField=document.getElementById('clearField');
-clearField.addEventListener('click',clear);
-for(let i=0;i<pixels.length;i++){
-    pixels[i].addEventListener('click',ticORtac);
-};
-let count=0;
-function ticORtac(){
-    const pixCont=this.innerHTML;
-        if(pixCont==''&&id!=true){
-            count++;
-            if(player==true){
-                this.innerHTML='X';
-                player=false;
-            }else{
-                this.innerHTML='0';
-                player=true;
-            }
-        };
-    check();
+let countOfMarkedCells = 0;
+let playerXwinsCounter = 0;
+let playerOwinsCounter = 0;
+let drawCounter        = 0;
+let endGame            = false;
+let player             = 'X';
+const cells            = document.getElementsByClassName('cell');
+
+for(let i=0; i<cells.length; i++){
+    cells[i].addEventListener('click',marksInOrderOfPriority);
 }
-function check(){
-    for(let j=0;j<winIndex.length;j++){
-        id=winIndex[j];
-        if(pixels[id[0]].innerHTML==pixels[id[1]].innerHTML&&pixels[id[1]].innerHTML==pixels[id[2]].innerHTML&&pixels[id[0]].innerHTML!=''){
-            for(i=0;i<3;i++){
-                pixels[id[i]].style.color='red';
-            };
-            id=true;
-            winTime++;
-            statistic();
-            break;
-        }
-    };
-    let both=true;
-    for(let i=0;i<pixels.length;i++){
-        if(pixels[i].innerHTML==''){
-           both=false; 
-        }
-    };
-    if(both==true&&id!=true&&winBothTime==0){
-        bothPlayers++;
-        winBothTime++;
-        const noWin=document.getElementById('draw');
-        noWin.innerHTML='draw:'+bothPlayers;
-    };
-};
-function clear(){
-    for(let i=0;i<pixels.length;i++){
-        pixels[i].style.color='black';
-        pixels[i].innerHTML='';
-    };
-    id=false;
-    player=true;
-    winTime=0;
-    winBothTime=0;
-};
-function statistic(){
-    if(winTime==1){
-        if(player==false){
-            playerX++;
-            const winX=document.getElementById('playerX');
-            winX.innerHTML='player X win:'+playerX;
+
+function marksInOrderOfPriority() {
+    const cellContent      = this.innerHTML;
+    if(cellContent=='' && endGame!=true){
+        countOfMarkedCells++;
+        if(player=='X'){
+            this.innerHTML = 'X';
+            player         = '0';
         }else{
-            playerO++;
-            const winO=document.getElementById('playerO');
-            winO.innerHTML='player 0 win:'+playerO;
-        };
-    }    
-};
+            this.innerHTML = '0';
+            player         = 'X';
+        }
+        let winner         = winningCheck();
+        statistic(winner);
+    }
+}
+
+function winningCheck(){
+    let winner
+    for(let j=0;j<winningPositions.length;j++){
+        let id                           = winningPositions[j];
+
+        if(cells[id[0]].innerHTML!='' && cells[id[0]].innerHTML==cells[id[1]].innerHTML && cells[id[1]].innerHTML==cells[id[2]].innerHTML){
+            winner                       = cells[id[0]].innerHTML;
+
+            for(let i=0;i<3;i++){
+                cells[id[i]].style.color = 'red';
+            }
+            endGame                      = true;
+            return winner;
+        }
+    }
+    if (countOfMarkedCells==9){
+        for(let i=0;i<cells.length;i++){
+            cells[i].style.color         = 'red';
+        }
+        endGame                          = true;
+        winner                           = 'draw';
+        return winner;
+    }
+}
+
+function statistic(winner){
+    if(endGame==true){
+        if (winner=='X') {
+            playerXwinsCounter++;
+            const winX      =document.getElementById('playerX');
+            winX.innerHTML  ='player X win:'+playerXwinsCounter;
+        } else if (winner=='0') {
+            playerOwinsCounter++;
+            const winO      =document.getElementById('playerO');
+            winO.innerHTML  ='player 0 win:'+playerOwinsCounter;
+        } else {
+            drawCounter++;
+            const draw     =document.getElementById('draw');
+            draw.innerHTML ='draw:'+drawCounter;
+        }
+    }
+}
+
+function clear(){
+    for(let i=0;i<cells.length;i++){
+        cells[i].style.color ='black';
+        cells[i].innerHTML   ='';
+    }
+    countOfMarkedCells       =0;
+    endGame                  =false;
+    player                   ='X';
+}
+const clearField =document.getElementById('clearField');
+clearField.addEventListener('click',clear);
