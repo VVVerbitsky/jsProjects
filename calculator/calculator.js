10fa4a6450509ae1219a4ef85e0a1cfa05a78f7c
Vadim Verbitsky <vvverbits@gmail.com>
2020-08-19 16:03:51 +0300

    'use strict' and let&const, comments for future fixes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,18 +1,22 @@
-var buttons=document.getElementsByClassName('button');
-var line=document.getElementById('line');
-var clear=document.getElementById('clear');
-var ecvals=document.getElementById('ecvals');
-for(var i=0;i<buttons.length;i++){
-    buttons[i].addEventListener('click',func);
+"use strict"
+const buttons = document.getElementsByClassName('button');
+const line    = document.getElementById('line');
+const clear   = document.getElementById('clear');
+const ecvals  = document.getElementById('ecvals');
+const operators = ['&divide', '*', '+', '-']; //unusable yet mb never?
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click', func);
 }
-clear.addEventListener('click',clearing);
-ecvals.addEventListener('click',result)
+clear.addEventListener('click', clearing);
+ecvals.addEventListener('click', result)
+
 function func(){ 
-   var type=this.getAttribute('data-type');
-   var inline=line.innerHTML;
-    line.innerHTML=line.innerHTML+type;
+    const type=this.getAttribute('data-type');
+    const inLine=line.innerHTML;
+    const lastSumbolInLine=inLine.charAt(inLine.length-1);
+    line.innerHTML=inLine+type;
     if(type=='/'||type=='*'||type=='-'||type=='+'){
-        if(inline.charAt(inline.length-1)=='/'||inline.charAt(inline.length-1)=='*'||inline.charAt(inline.length-1)=='+'||inline.charAt(inline.length-1)=='-'){
+        if(lastSumbolInLine=='/'||lastSumbolInLine=='*'||lastSumbolInLine=='+'||lastSumbolInLine=='-'){
             line.innerHTML='error';
         }   
     };
@@ -24,7 +28,7 @@ function clearing(){
     line.innerHTML='';
 }
 function result(){
-    line.innerHTML=eval(line.innerHTML);
+    line.innerHTML=eval(line.innerHTML); //should fix this and after that fix '/' on screen to '&divide'
     if(line.innerHTML.length>20){
         line.innerHTML='too long';
     };
