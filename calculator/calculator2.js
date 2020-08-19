b13c9e3f78cf5cb6a82d25d416f9a6ad9d6f50e2
Vadim Verbitsky <vvverbits@gmail.com>
2020-08-19 18:05:01 +0300

    add second .js totally new js code file(instead of old in future)

---
+++ calculator/calculator2.js
@@ -0,0 +1,80 @@
+"use strict"
+const line      = document.getElementById('line');
+
+const numbers   = document.getElementsByClassName('number');
+for(let i=0;i<numbers.length;i++){
+    numbers[i].addEventListener('click', addNumber);
+}
+const operators = document.getElementsByClassName('operator');
+for(let i=0;i<operators.length;i++){
+    operators[i].addEventListener('click', addOperator);
+}
+const clear     = document.getElementById('clear');
+clear.addEventListener('click', clearLine);
+
+let firstNumber=0, secondNumber=0, udefindNumber=0; //firstNumber operator secondNumber = result
+let accessForOperator = true;
+let accessForNumber   = true;
+let operator;
+//const operators = ['/', '*', '+', '-']; //unusable yet mb never?
+function addNumber(){ 
+    const type=this.getAttribute('data-type');
+    const inLine=line.innerHTML;
+    if (accessForNumber==true){
+        if (inLine=='0'){
+            line.innerHTML = type
+            
+        } else {
+            line.innerHTML = inLine+type;
+        }
+        accessForOperator  = true;
+    } else {
+        line.innerHTML     = type;
+        accessForNumber    = true;
+    }
+    if(line.innerHTML.length>20){
+        line.innerHTML     = 'too long';
+    }
+}   
+function addOperator(){
+    operator=this.getAttribute('data-type');
+    udefindNumber=line.innerHTML;
+    accesForNumber=false;
+
+
+}
+//     const lastSumbolInLine=inLine.charAt(inLine.length-1);
+//     line.innerHTML=inLine+type;
+//     if(type=='/'||type=='*'||type=='-'||type=='+'){
+//         if(lastSumbolInLine=='/'||lastSumbolInLine=='*'||lastSumbolInLine=='+'||lastSumbolInLine=='-'){
+//             line.innerHTML='error';
+//         }   
+//     };
+//     if(line.innerHTML.length>20){
+//         line.innerHTML='too long';
+//     };
+// }
+
+function clearLine(){
+    line.innerHTML='0';
+}
+function result(){
+    switch (operator){
+        case '/':
+            line.innerHTML=firstNumber/secondNumber;
+            break;
+        case '+':
+            line.innerHTML=firstNumber+secondNumber;
+            break;
+        case '-':
+            line.innerHTML=firstNumber-secondNumber;
+            break;   
+        case '*':
+            line.innerHTML=firstNumber*secondNumber;
+            break;       
+    }
+    
+    if(line.innerHTML.length>20){
+        line.innerHTML='too long';
+    };
+}
