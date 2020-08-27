81529f48b4bfbd68247c53b026c3430fa15e13c8
Vadim Verbitsky <vvverbits@gmail.com>
2020-08-27 20:41:51 +0300

    almost completely new content of functions, fixed 0.1+0.2; /0; too long numbers

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,7 @@
 "use strict"
+//bugs:
+//first pressed button is '.';
+//bug with impossible '0.13.2.33 numbers'
 const line      = document.getElementById('line');
 
 const numbers   = document.getElementsByClassName('number');
@@ -10,68 +13,124 @@ for(let i=0;i<operators.length;i++){
     operators[i].addEventListener('click', addOperator);
 }
 const clear     = document.getElementById('clear');
-clear.addEventListener('click', clearLine);
+clear.addEventListener('click', clearAll);
 const ecvals     = document.getElementById('ecvals');
 ecvals.addEventListener('click', result);
-let firstNumber=0;
-let secondNumber=0;
-let onScreenNumber    = 0;
-let offScreenNumber   = 0;
-let accessForOperator = true;
-let continueNumber    = false;
-let operator='empty';
-let hardNumberPrepareToSave=false;
-let hardNumber;
-//const operators = ['/', '*', '+', '-']; //unusable yet mb never?
-function addNumber(){ 
+let hardNumber=0;
+let softNumber=0;
+let number;
+let saveTOhard=false;
+let previousEventISaddNumber;
+let operator=null;
+function addNumber(){
+    if(line.innerHTML=='too long'){
+        clearAll();
+    } 
     const type=this.getAttribute('data-type');
-    const inLine=line.innerHTML;
-    if (continueNumber==true){
-        line.innerHTML = inLine+type;
-    } else {
-        line.innerHTML     = type;
-        continueNumber    = true;
+    console.log(previousEventISaddNumber);
+    if(previousEventISaddNumber==true){
+        line.innerHTML=line.innerHTML+type;
+    } else{
+        line.innerHTML=type;
     }
-    if(line.innerHTML.length>20){
-        line.innerHTML     = 'too long';
+    previousEventISaddNumber=true;
+    number=+line.innerHTML;
+    if(line.innerHTML.length>21){
+        line.innerHTML='too long';
+    }
+    if (saveTOhard==true){
+        hardNumber=number;
+    } else {
+        softNumber=number;
     }
 }   
 function addOperator(){
+    if(line.innerHTML=='too long'){
+        clearAll();
+    } 
     operator=this.getAttribute('data-type');
-    offScreenNumber=+line.innerHTML;
-    firstNumber=offScreenNumber;
-    continueNumber=false;
-    hardNumberPrepareToSave=true;
+    previousEventISaddNumber=false;
+    saveTOhard=true;
+
 
 }
-function clearLine(){
+function clearAll(){
     line.innerHTML='0';
-    operator='empty';
-    continueNumber   = false;
+    hardNumber=0;
+    softNumber=0;
+    number=undefined;
+    saveTOhard=false;
+    previousEventISaddNumber=undefined;
+    operator=undefined;
 }
 
-function result(){
-    onScreenNumber=+line.innerHTML;
-    secondNumber=onScreenNumber;
-    console.log(offScreenNumber,' ',operator,' ',onScreenNumber)
-    switch (operator){
-        case '/':
-            line.innerHTML=offScreenNumber/onScreenNumber;
-            break;
-        case '+':
-            line.innerHTML=offScreenNumber+onScreenNumber;
-            break;
-        case '-':
-            line.innerHTML=offScreenNumber-onScreenNumber;
-            break;   
-        case '*':
-            line.innerHTML=offScreenNumber*onScreenNumber;
-            break;       
+function fractionalCheck(){
+    let fractionalLength=0;
+    if (leftNumber%1!=0){
+        fractionalLength=(leftNumber+'').split('.').pop().length;
+        leftNumber=leftNumber*10**fractionalLength;
+        rightNumber=rightNumber*10**fractionalLength;
+        
     }
-    
-    if(line.innerHTML.length>20){
-        line.innerHTML='too long';
-    };
-    offScreenNumber=+line.innerHTML;
-    continueNumber=false;
+    if (rightNumber%1!=0){
+        fractionalLength=fractionalLength+(rightNumber+'').split('.').pop().length;
+        leftNumber=leftNumber*10**fractionalLength;
+        rightNumber=rightNumber*10**fractionalLength;
+    }
+    return fractionalLength;
+}
+function result(){
+    if(operator!=undefined){
+        function fractionalCheck(){
+            let fractionalLength=0;
+            if (leftNumber%1!=0){
+                fractionalLength=(leftNumber+'').split('.').pop().length;
+                leftNumber=leftNumber*10**fractionalLength;
+                rightNumber=rightNumber*10**fractionalLength;
+                
+            }
+            if (rightNumber%1!=0){
+                fractionalLength=fractionalLength+(rightNumber+'').split('.').pop().length;
+                leftNumber=leftNumber*10**fractionalLength;
+                rightNumber=rightNumber*10**fractionalLength;
+            }
+            return fractionalLength;
+        }
+        let leftNumber=softNumber;
+        let rightNumber=hardNumber;
+
+        let answer;
+        let fractionalLength=fractionalCheck();
+        
+        console.log(softNumber,' ',operator,' ',hardNumber);
+        switch (operator){
+            case '/':
+                console.log(rightNumber)
+                if(rightNumber==0){
+                    console.log('test');
+                    line.innerHTML='Cannot divide by zero';
+                    return
+                } else {
+                    answer=leftNumber/rightNumber
+                    break;
+                }
+            case '+':
+                answer=leftNumber+rightNumber;
+                break;
+            case '-':
+                answer=leftNumber-rightNumber;
+                break;   
+            case '*':
+                answer=leftNumber*rightNumber;
+                break;       
+        }
+        answer=answer/10**fractionalLength;
+        line.innerHTML=answer;
+        softNumber=answer;
+        previousEventISaddNumber=false;
+        saveTOhard=false;
+        if(line.innerHTML.length>21){
+            line.innerHTML='too long';
+        }
+    }
 }
