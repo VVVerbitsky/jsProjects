def7f9d51658e81e5655b6b4dfe359c4dcea987a
Vadim Verbitsky <vvverbits@gmail.com>
2020-08-31 15:31:04 +0300

    animation work in process

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,17 +1,36 @@
 "use strict"
-colorsLibrary={2:   'rgb(238, 228, 218)',
-               4:   'rgb(237, 224, 200)',
-               8:   'rgb(242, 177, 121)',
-               16:  'rgb(245, 149, 99)',
-               32:  'rgb(246, 124, 95)',
-               64:  'rgb(246, 94, 59)',
-               128: 'rgb(237, 207, 114)',
-               256: 'rgb(237, 204, 97)',
-               512: 'rgb(237, 200, 280)',
-               1024:'rgb(237, 197, 63)',
-               2048:'rgb(237, 224, 200)',             
-}
 !function(){
+    let colorsLibrary={ 2:   'rgb(238, 228, 218)',
+                        4:   'rgb(237, 224, 200)',
+                        8:   'rgb(242, 177, 121)',
+                        16:  'rgb(245, 149, 99)',
+                        32:  'rgb(246, 124, 95)',
+                        64:  'rgb(246, 94, 59)',
+                        128: 'rgb(237, 207, 114)',
+                        256: 'rgb(237, 204, 97)',
+                        512: 'rgb(237, 200, 280)',
+                        1024:'rgb(237, 197, 63)',
+                        2048:'rgb(237, 224, 200)'}
+    document.onkeydown=function(arrowKey){
+        switch(arrowKey.keyCode){
+            case 37:
+                filledCell.style.marginLeft=+filledCell.style.marginLeft.split('px')[0]-110+'px';
+                console.log('left');
+                break;
+            case 38:
+                filledCell.style.marginTop=+filledCell.style.marginTop.split('px')[0]-110+'px';
+                console.log('up');
+                break;
+            case 39:
+                filledCell.style.marginLeft=+filledCell.style.marginLeft.split('px')[0]+110+'px';
+                console.log('right');
+                break;
+            case 40:
+                filledCell.style.marginTop=+filledCell.style.marginTop.split('px')[0]+110+'px';
+                console.log('down');
+                break;
+        }
+    }
     function getRandomInt(min,max){
         return Math.floor(Math.random()*(max-min+1))+min;
     }
@@ -29,6 +48,14 @@ colorsLibrary={2:   'rgb(238, 228, 218)',
         }
     }
     drawField(4, 4);
+    let cell=document.getElementById('1,2');
+    let filledCell=document.createElement('div');
+    filledCell.className='filled';
+    filledCell.style.backgroundColor='rgb(238, 228, 218)';
+    filledCell.style.color='rgb(119, 110, 101)'
+    filledCell.innerHTML='2';
+    cell.appendChild(filledCell);
+
 
 
 
