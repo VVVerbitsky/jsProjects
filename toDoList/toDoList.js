94ec03991dde643169d1eb4d18f207fbd1d8f484
VVVerbitsky <vvverbits@gmail.com>
2021-03-01 17:38:49 +0300

    format document

--- toDoList/toDoList.js
+++ toDoList/toDoList.js
@@ -1,15 +1,15 @@
-!function(){
-    const textarea=document.getElementsByTagName('textarea')[0];
-    const addButton=document.getElementsByClassName('add')[0];
-    const toDoPull=document.getElementsByClassName('toDoPull')[0];
-    const donePull=document.getElementsByClassName('donePull')[0];
+!function () {
+    const textarea = document.getElementsByTagName('textarea')[0];
+    const addButton = document.getElementsByClassName('add')[0];
+    const toDoPull = document.getElementsByClassName('toDoPull')[0];
+    const donePull = document.getElementsByClassName('donePull')[0];
 
-    function createTask(doList,doneList,textSource){
-        if(textSource!=''){
-            textarea.value='';
+    function createTask(doList, doneList, textSource) {
+        if (textSource != '') {
+            textarea.value = '';
 
-            const task=document.createElement('div');
-            task.className='toDoElem';
+            const task = document.createElement('div');
+            task.className = 'toDoElem';
 
             task.appendChild(createCheckbox(task, doList, doneList));
             task.appendChild(createTaskText(textSource));
@@ -17,38 +17,38 @@
 
             doList.insertBefore(task, doList.firstChild);
 
-        } else{ alert('too easy task for you')}
+        } else { alert('too easy task for you') }
     }
 
-    function createCheckbox(task, doList, doneList){
-        const checkbox=document.createElement('input');
-            checkbox.type='checkbox';
+    function createCheckbox(task, doList, doneList) {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
 
-            checkbox.addEventListener('click',()=>{
-                if(!checkbox.checked){
-                    doList.appendChild(task);
-                    task.className='toDoElem';
-                }else{
-                    task.className='toDoElem done';
-                    doneList.appendChild(task);
-                }
-            });
+        checkbox.addEventListener('click', () => {
+            if (!checkbox.checked) {
+                doList.appendChild(task);
+                task.className = 'toDoElem';
+            } else {
+                task.className = 'toDoElem done';
+                doneList.appendChild(task);
+            }
+        });
         return checkbox;
     }
 
-    function createTaskText(textSource){
-        const taskText=document.createElement('p');
-        taskText.innerHTML=textSource;
+    function createTaskText(textSource) {
+        const taskText = document.createElement('p');
+        taskText.innerHTML = textSource;
         return taskText;
     }
 
-    function createDeleteButton(task){
-        const deleteButton=document.createElement('div');
-        deleteButton.className='delete';
-        deleteButton.innerHTML='delete';
-        deleteButton.addEventListener('click',()=>{task.remove()});
+    function createDeleteButton(task) {
+        const deleteButton = document.createElement('div');
+        deleteButton.className = 'delete';
+        deleteButton.innerHTML = 'delete';
+        deleteButton.addEventListener('click', () => { task.remove() });
         return deleteButton;
     }
 
-    addButton.addEventListener('click', ()=>{createTask(toDoPull, donePull, textarea.value)});
+    addButton.addEventListener('click', () => { createTask(toDoPull, donePull, textarea.value) });
 }();
