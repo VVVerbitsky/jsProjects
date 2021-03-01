a8ee735313d4f29522cac67aa02d5d8e2d7696f7
VVVerbitsky <vvverbits@gmail.com>
2021-03-01 17:28:30 +0300

    main part of code is split into separate functions

--- toDoList/toDoList.js
+++ toDoList/toDoList.js
@@ -1,37 +1,54 @@
 !function(){
-    let toDoPull=document.getElementsByClassName('toDoPull')[0];
-    let completedPull=document.getElementsByClassName('completedPull')[0];
     let textarea=document.getElementsByTagName('textarea')[0];
     let addButton=document.getElementsByClassName('add')[0];
-    addButton.addEventListener('click', ()=>{createTask(toDoPull,completedPull,textarea.value)});
-    console.log(completedPull);
-    console.log( toDoPull);
-    console.log(textarea);
+    let toDoPull=document.getElementsByClassName('toDoPull')[0];
+    let donePull=document.getElementsByClassName('donePull')[0];
+
     function createTask(doList,doneList,textSource){
         if(textSource!=''){
-        let elem=document.createElement('div');
-        elem.className='toDoElem';
-        let input=document.createElement('input');
-        input.type='checkbox';
-        input.addEventListener('click',()=>{
-            if(input.checked==true){
-                elem.className='toDoElem completed';
-                doneList.appendChild(elem);
-        }else{doList.appendChild(elem);
-              elem.className='toDoElem';
-        }});
-        elem.appendChild(input);
-        let p=document.createElement('p');
-        p.innerHTML=textSource;
-        elem.appendChild(p);
+            textarea.value='';
+
+            let task=document.createElement('div');
+            task.className='toDoElem';
+
+            task.appendChild(createCheckbox(task, doList, doneList));
+            task.appendChild(createTaskText(textSource));
+            task.appendChild(createDeleteButton(task));
+
+            doList.insertBefore(task, doList.firstChild);
+
+        } else{ alert('too easy task for you')}
+    }
+
+    function createCheckbox(task, doList, doneList){
+        let checkbox=document.createElement('input');
+            checkbox.type='checkbox';
+
+            checkbox.addEventListener('click',()=>{
+                if(!checkbox.checked){
+                    doList.appendChild(task);
+                    task.className='toDoElem';
+                }else{
+                    task.className='toDoElem done';
+                    doneList.appendChild(task);
+                }
+            });
+        return checkbox;
+    }
+
+    function createTaskText(textSource){
+        let taskText=document.createElement('p');
+        taskText.innerHTML=textSource;
+        return taskText;
+    }
+
+    function createDeleteButton(task){
         let deleteButton=document.createElement('div');
         deleteButton.className='delete';
         deleteButton.innerHTML='delete';
-        elem.appendChild(deleteButton);
-        // doList.appendChild(elem);
-        doList.insertBefore(elem,doList.firstChild);
-        deleteButton.addEventListener('click',()=>{elem.remove()});
-        textarea.value='';
-        } else{ alert('too easy task for you')}
+        deleteButton.addEventListener('click',()=>{task.remove()});
+        return deleteButton;
     }
+
+    addButton.addEventListener('click', ()=>{createTask(toDoPull, donePull, textarea.value)});
 }();
